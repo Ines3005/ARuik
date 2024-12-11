571ea05846925cc26fc4290f48b01b2a4b25ed34
zayani ines <zayaniines011@gmail.com>
2024-12-11 12:48:57 +0100

    push ar code

---
+++ Assets/VbAnim104.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VbAnim104 : MonoBehaviour
+{
+    public GameObject classImage; // L'image de la classe
+    public GameObject classText;  // Le texte associé
+
+    // Start est appelé au début
+    void Start()
+    {
+        // Cache l'image et le texte au début
+        classImage.SetActive(false);
+        classText.SetActive(false);
+    }
+
+    // Appelé lorsqu'un événement se produit (exemple : détection d'un marqueur)
+    public void ShowClassInfo()
+    {
+        // Affiche l'image et le texte
+        classImage.SetActive(true);
+        classText.SetActive(true);
+
+        Debug.Log("La classe 104 est affichée.");
+    }
+
+    // Méthode pour cacher les éléments si nécessaire
+    public void HideClassInfo()
+    {
+        classImage.SetActive(false);
+        classText.SetActive(false);
+
+        Debug.Log("La classe 104 est cachée.");
+    }
+}
+
